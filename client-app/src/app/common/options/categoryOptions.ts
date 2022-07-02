af4d7fae7d1f67f0658fb1cd454200cd35545d9d
bardha cocaj <fc52610@ubt-uni.net>
2022-07-03 01:20:07 +0200

    validimi

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Novele', value: 'novele'},
+    {text: 'Roman', value: 'roman'},
+    {text: 'Shkencore', value: 'shkencore'},
+    {text: 'Femije', value: 'femije'},
+    {text: 'Perralla', value: 'perralla'},
+    {text: 'Enciklopedi', value: 'enciklopedi'},
+] 
--- client-app/src/features/books/form/BookForm.tsx
+++ client-app/src/features/books/form/BookForm.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
-import { Button, Form, Segment } from 'semantic-ui-react';
+import { Button, Header, Segment } from 'semantic-ui-react';
 import { Book } from '../../../app/models/book';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
@@ -7,6 +7,13 @@ import { Link, Navigate, useParams } from 'react-router-dom';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { v4 as uuid } from 'uuid';
 import {useNavigate } from 'react-router-dom';
+import { Formik , Form } from 'formik';
+import { values } from 'mobx';
+import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from '../../../app/common/form/MyTextArea';
+import { categoryOptions } from '../../../app/common/options/categoryOptions';
+import MySelectInput from '../../../app/common/form/MySelectInput';
 
 
 export default observer(function BookForm() {
@@ -15,7 +22,7 @@ const {bookStore}= useStore();
 const {createBook, updateBook,loading,loadBook,loadingInitial}=bookStore;
 const {id} = useParams<{id: string}>();
 
-const [book, setBook] = useState({
+const [book, setBook] = useState<Book>({
     id: '',
     titulli: '',
     kategoria: '',
@@ -27,44 +34,64 @@ const [book, setBook] = useState({
 
 });
 
+const validationSchema = Yup.object({
+    titulli: Yup.string().required('The book title is required'),
+    pershkrimi: Yup.string().required('The book description is required'),
+    kategoria: Yup.string().required(),
+    faqet: Yup.string().required(),
+    cmimi: Yup.string().required(),
+    botuesi: Yup.string().required(),
+    disponueshmeria: Yup.string().required(),
+})
+
 useEffect(() => {
     if (id) loadBook(id).then(book => setBook(book!))
 }, [id, loadBook]);
 
 
-    function handleSubmit() {
-       if (book.id.length === 0) {
-        let newBook = {
-            ...book,
-            id: uuid()
-        };
-        createBook(newBook).then(() => navigate(`/books/${newBook.id}`))
-    } else {
-        updateBook(book).then(() => navigate(`/books/${book.id}`))
-    }
-}
+     function handleFormSubmit(book : Book) {
+        if (book.id.length === 0) {
+         let newBook = {
+             ...book,
+             id: uuid()
+         };
+         createBook(newBook).then(() => navigate(`/books/${newBook.id}`))
+     } else {
+         updateBook(book).then(() => navigate(`/books/${book.id}`))
+     }
+ }
 
-    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        const {name, value} = event.target;
-        setBook({...book, [name]: value})
-    }
 
     if (loadingInitial) return <LoadingComponent content='Loading book...' />
-
+       
     return (
         <Segment clearing>
-            <Form onSubmit={handleSubmit} autoComplete='off'>
-                <Form.Input placeholder='Titulli' value={book.titulli} name='titulli' onChange={handleInputChange} />
-                <Form.TextArea placeholder='Pershkrimi' value={book.pershkrimi} name='pershkrimi' onChange={handleInputChange} />
-                <Form.Input placeholder='Kategoria' value={book.kategoria} name='kategoria' onChange={handleInputChange} />
-                <Form.Input placeholder='Faqet' value={book.faqet} name='faqet' onChange={handleInputChange} />
-                <Form.Input placeholder='Cmimi' value={book.cmimi} name='cmimi' onChange={handleInputChange}/>
-                <Form.Input placeholder='Botuesi' value={book.botuesi} name='botuesi' onChange={handleInputChange}/>
-                <Form.Input placeholder='Disponueshmeria' value={book.disponueshmeria} name='disponueshmeria' onChange={handleInputChange}/>
-                <Button floated='right' positive type='submit' content='Submit' />
-                <Button loading={loading} floated='right' positive type='submit' content='Submit' />
-                <Button as={Link} to='/books' floated='right' type='button' content='Cancel' />
-            </Form>
-        </Segment>
+            <Header content='Book Details' sub color='teal'/>
+            <Formik  
+            validationSchema={validationSchema}
+            enableReinitialize 
+            initialValues={book} 
+            onSubmit={values => handleFormSubmit(values)}>
+            {({ handleSubmit, isValid , isSubmitting, dirty}) => (
+        <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                 
+            <MyTextInput placeholder='Titulli' name='titulli'  />
+            <MyTextArea rows={3} placeholder='Pershkrimi'  name='pershkrimi'  />
+            <MySelectInput options={categoryOptions} placeholder='Kategoria'  name='kategoria'  />
+            <MyTextInput placeholder='Faqet'  name='faqet'  />
+            <MyTextInput placeholder='Cmimi'  name='cmimi' />
+            <MyTextInput placeholder='Botuesi' name='botuesi' />
+            <MyTextInput placeholder='Disponueshmeria' name='disponueshmeria' />
+            <Button
+             disabled={isSubmitting || !dirty || !isValid} 
+             loading={loading} floated='right' 
+             positive type='submit' content='Submit' />
+            <Button as={Link} to='/books' floated='right' type='button' content='Cancel' />
+       </Form>
+            )}
+
+      </Formik>
+           
+      </Segment>
     )
 } )
